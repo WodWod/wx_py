3afe47b62b6d61ece9afffe1a58aaa5810086cf7
su <1213663689@qq.com>
2019-12-30 09:22:36 +0800

    12.30

--- handle.py
+++ handle.py
@@ -22,14 +22,16 @@ class Handle(object):
             list= [token,timestamp,nonce]
             list.sort()
             sha1=hashlib.sha1()
-            sha1.update,list[0]
-            sha1.update,list[1]
-            sha1.update,list[2]
+            sha1.update(list[0].encode('utf-8'))
+            sha1.update(list[1].encode('utf-8'))
+            sha1.update(list[2].encode('utf-8'))
             hashcode = sha1.hexdigest()
             print('handle/GET func: hashcode, signature: ',hashcode,signature)
             if hashcode==signature:
+                print("success")
                 return echostr
             else:
+                print("fail")
                 return ''
         except BaseException as e:
             print('Error:',e)
